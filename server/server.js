7f1790d899d79fa1881036b83ad4bf4dfa6161f9
ThinkFWD <mike.thinkfwd@gmail.com>
2017-04-18 11:28:24 -0400

    try #2

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const port =  (process.env.PORT || 8888);
+const ports =  (process.env.PORT || 8888);
 
 dotenv.config();
 
@@ -12,16 +12,17 @@ const app = express();
 app.use(cors());
 require('./config/routes.js')(app, express);
 
-app.use('/build', express.static(path.join(`${__dirname}/../build`)));
-app.use(express.static(path.join(`${__dirname}/../public`)));
+app.use('/build', express.static(path.join(__dirname, '/../build')));
+app.use(express.static(path.join(__dirname, '/../public')));
 
+console.log('HELLO WOLRD', __dirname);
 
 app.get('*', (request, response) => {
   response.sendFile(path.resolve(`${__dirname}/../public`, 'index.html'));
 });
 
-const server = app.listen(port, () => {
+const server = app.listen(ports, () => {
   const host = server.address().address;
   const port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, ports);
 });
