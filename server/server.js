968505235db3ca16e2865f6bb0e18d1041631c5b
ThinkFWD <mike.thinkfwd@gmail.com>
2017-04-18 14:53:24 -0400

    beta 10

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const ports =  (process.env.PORT || 8888);
+const port =  (process.env.PORT || 8888);
 
 dotenv.config();
 
@@ -12,17 +12,16 @@ const app = express();
 app.use(cors());
 require('./config/routes.js')(app, express);
 
-app.use(express.static(path.join(__dirname, '/../build')));
-app.use(express.static(path.join(__dirname, '/../public')));
+app.use(express.static(path.join(`${__dirname}/../build`)));
+app.use(express.static(path.join(`${__dirname}/../public`)));
 
 
 app.get('*', (request, response) => {
-  console.log('WE NEED TO SERVE', request.url);
   response.sendFile(path.resolve(`${__dirname}/../public`, 'index.html'));
 });
 
-const server = app.listen(ports, () => {
+const server = app.listen(port, () => {
   const host = server.address().address;
   const port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, ports);
+  console.log('Example app listening at http://%s:%s', host, port);
 });
