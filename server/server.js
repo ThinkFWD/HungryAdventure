c4ff06e5083811a3356369c3e952253f9c2215b6
ThinkFWD <mike.thinkfwd@gmail.com>
2017-04-18 11:12:06 -0400

    betaTest

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const port =  (process.env.PORT || 8888);
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const app = express();
 app.use(cors());
 require('./config/routes.js')(app, express);
 
-app.use(express.static(path.join(`${__dirname}/../build`)));
+app.use('/build', express.static(path.join(`${__dirname}/../build`)));
 app.use(express.static(path.join(`${__dirname}/../public`)));
 
 
@@ -19,7 +20,7 @@ app.get('*', (request, response) => {
   response.sendFile(path.resolve(`${__dirname}/../public`, 'index.html'));
 });
 
-const server = app.listen(process.env.PORT || 8888, () => {
+const server = app.listen(port, () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './client/src/client.js',
@@ -12,8 +13,23 @@ module.exports = {
 
   devtool: 'source-map',
 
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+  ],
+
   module: {
-    rules: [
+    loaders: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
